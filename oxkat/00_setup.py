c8bace2eab96e5a54cf2a42394b7bd5fc11e5c5d
Ian Heywood <IanHeywood@users.noreply.github.com>
2019-01-22 20:20:37 +0000

    Update 00_setup.py

--- oxkat/00_setup.py
+++ oxkat/00_setup.py
@@ -7,29 +7,43 @@ import numpy
 import sys
 import glob
 import pickle
+from astropy.coordinates import SkyCoord
+
+
+def calcsep(ra0,dec0,ra1,dec1):
+    c1 = SkyCoord(ra0+'deg',dec0+'deg',frame='fk5')
+    c2 = SkyCoord(ra1+'deg',dec0+'deg',frame='fk5')
+    sep = c1.separartion(c2)
+    return sep.value
 
 
 myms = sys.argv[1].rstrip('/')
 
 
+# Positions and labels for the preferred primary calibrators
+
 cals = [('1934',294.85427795833334,-63.71267375),
     ('0408',62.084911833333344,-65.75252238888889)]
 
 
-#myms = glob.glob('*pol.ms')[0]
+# Last three digits of the SB ID, for slurm queue labels, etc.
 code = myms.split('/')[-1].split('_')[0][-3:]
 
 
+# LO and HI Measurement Sets (might ditch this..)
 loms = myms.replace('.ms','_LO_wtspec.ms')
 hims = hims = myms.replace('.ms','_HI_wtspec.ms')
 
 
+# Get the number of channels
 spw_table = table(myms+'/SPECTRAL_WINDOW',ack=False)
 nchan = spw_table.getcol('NUM_CHAN')[0]
 spw_table.close()
 
 
-edge_flags = int(150*nchan/4096)
+# Set the number of edge flags scaled by the number of channels
+# Set the channels over which to solve for delays
+edge_flags = int(120*nchan/4096)
 k0 = int(2120*nchan/4096)
 k1 = int(3120*nchan/4096)
 
@@ -37,7 +51,7 @@ k1 = int(3120*nchan/4096)
 project_info = {'primary':['0','0'],
     'secondary':['1','1'],
     'target':['2','2'],
-    'primary_name':'',
+    'primary_name':'UNKNOWN',
     'ref_ant':'-1',
     'master_ms':myms,
     'lo_ms':loms,
@@ -80,6 +94,10 @@ for i in range(0,len(names)):
     if state == primary_state:
         project_info['primary'] = [names[i],str(i)]
         primary_dir = dirs[i][0,:]*180.0/numpy.pi
+        for cal in cals:
+            sep = calcsep(primary_dir[0],primary_dir[1],cal[1],cal[2])
+            if sep < 1e-4 and project_info['primary_name'] == 'UNKNOWN':
+                project_info['primary_name'] = cal[0]
     if state == secondary_state:
         project_info['secondary'] = [names[i],str(i)]
     if state == target_state:

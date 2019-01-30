65bc7cdc33378d0c310ecb97d211e85e363d8c28
IanHeywood <ian.heywood@gmail.com>
2019-01-30 12:10:51 +0000

    add LO/HI split script

--- oxkat/01_casa_make_LO_HI_ms.py
+++ oxkat/01_casa_make_LO_HI_ms.py
@@ -21,54 +21,18 @@ def ranges(nums):
 
 myms = project_info['master_ms']
 edge_flags = project_info['edge_flags']
-rfimask = 'labelled_rfimask.5jun17.pickle.npy'
 
-
-if not os.path.isfile(rfimask):
-	os.system('wget http://www-astro.physics.ox.ac.uk/~ianh/labelled_rfimask.5jun17.pickle.npy')
-
-
-flags = numpy.load(rfimask)
-ref_freqs = flags[1]['chans']
-ref_flags = flags[0]['chans']
 
 tb.open(myms+'/SPECTRAL_WINDOW')
 ms_freqs = tb.getcol('CHAN_FREQ')
-ms_flags = []
 
 
 clipfactor = 4096.0/float(len(ms_freqs))
 lowclip = int(1280.0/clipfactor)
 
 
-for i in range(0,len(ms_freqs)-1):
-	f0 = ms_freqs[i]
-	f1 = ms_freqs[i+1]
-	mask = (ref_freqs >= f0) & (ref_freqs < f1)
-	temp = ref_flags[mask]
-	if 1 in temp:
-		ms_flags.append(i)
-
-
-#pc = str(100.0*round(float(len(ms_flags))/float(len(ms_freqs)),2))
-chanranges = ranges(ms_flags)
-
-
-spwstr = '0:'
-for i in range(0,len(chanranges)):
-	tmpstr = str(chanranges[i][0])+'~'+str(chanranges[i][1])
-	if i != (len(chanranges) - 1):
-		tmpstr += ';'
-	spwstr += tmpstr
-
-
 mid_chan = int(len(ms_freqs)/2.0)
 end_chan = int(len(ms_freqs))-edge_flags
-
-
-flagdata(vis=myms,mode='manual',spw=spwstr)
-flagdata(vis=myms,mode='manual',autocorr=True)
-flagdata(vis=myms,mode='clip',clipzeros=True)
 
 
 loms = project_info['lo_ms']
@@ -84,11 +48,6 @@ mstransform(vis=myms,
 	usewtspectrum=True)
 
 
-flagdata(vis=loms,mode='clip',clipminmax=[0.0,50.0])
-flagdata(vis=loms,mode='tfcrop')
-flagdata(vis=loms,mode='extend',growaround=True,flagneartime=True,flagnearfreq=True,growtime=90.0,growfreq=90.0)
-
-
 hims = project_info['hi_ms']
 
 
@@ -100,11 +59,6 @@ mstransform(vis=myms,
         timeaverage=False,
         realmodelcol=True,
         usewtspectrum=True)
-
-
-flagdata(vis=hims,mode='clip',clipminmax=[0.0,50.0])
-flagdata(vis=hims,mode='tfcrop')
-flagdata(vis=hims,mode='extend',growaround=True,flagneartime=True,flagnearfreq=True,growtime=90.0,growfreq=90.0)
 
 
 if not os.path.isdir('LO'):

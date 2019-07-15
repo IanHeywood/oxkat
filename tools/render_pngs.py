93559f785e16fbb2158942e6024c99a329bf27c0
IanHeywood <ian.heywood@gmail.com>
2019-07-15 16:31:43 +0100

    Indentation

--- tools/render_pngs.py
+++ tools/render_pngs.py
@@ -6,20 +6,21 @@ from oxkat.generate_jobs import *
 
 def main():
 
-	pattern = sys.argv[1]
 
-	gen.setup_dir(SCRIPTS)
-	gen.setup_dir(PNGS)
+    pattern = sys.argv[1]
 
-	fitslist = glob.glob(pattern)
+    gen.setup_dir(SCRIPTS)
+    gen.setup_dir(PNGS)
+
+    fitslist = glob.glob(pattern)
 
     slurmfile = SCRIPTS+'/slurm_render_pngs.sh'
     logfile = LOGS+'/slurm_render_pngs.log'
 
-	syscall = ''
+    syscall = ''
 
-	for infits in fitslist:
-		syscall += generate_syscall_mviewer(infits)
+    for infits in fitslist:
+        syscall += generate_syscall_mviewer(infits)
 
     gen.write_slurm(opfile=slurmfile,
             jobname='makepngs',

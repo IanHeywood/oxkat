4cbfab45ea4c0f101070c37d63183f5e71fb6634
IanHeywood <ian.heywood@gmail.com>
2019-07-24 11:27:33 +0100

    formatting

--- oxkat/casa_tfcrop_cals_corrected.py
+++ oxkat/casa_tfcrop_cals_corrected.py
@@ -5,11 +5,9 @@ import pickle
 
 
 project_info = pickle.load(open('project_info.p','rb'))
+myms = project_info['master_ms']
 bpcal = project_info['primary']
 pcal = project_info['secondary']
-
-
-myms = project_info['master_ms']
 
 
 flagdata(vis=myms,mode='tfcrop',datacolumn='corrected',field=bpcal[1])

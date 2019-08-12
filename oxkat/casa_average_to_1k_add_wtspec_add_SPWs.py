abfd0909ccaf9ce67c42d474373b3ba0d83d2f82
IanHeywood <ian.heywood@gmail.com>
2019-08-12 16:34:39 +0100

    SPWs

---
+++ oxkat/casa_average_to_1k_add_wtspec_add_SPWs.py
@@ -0,0 +1,29 @@
+# ian.heywood@physics.ox.ac.uk
+
+import glob
+
+myms = glob.glob('*.ms')[0]
+opms = myms.replace('.MS','.ms')
+opms = myms.replace('.ms','_wtspec.ms')
+
+tb.open(myms+'/SPECTRAL_WINDOW')
+nchan = tb.getcol('NUM_CHAN')[0]
+tb.done()
+
+mychanbin = int(nchan/1024)
+if mychanbin == 1:
+	mychanave = False
+else:
+	mychanave = True
+
+mstransform(vis=myms,
+	outputvis=opms,
+	datacolumn='data',
+	chanaverage=mychanave,
+	chanbin=mychanbin,
+	timeaverage=True,
+	timebin='8s',
+	realmodelcol=True,
+	usewtspectrum=True)
+
+flagmanager(vis=opms,mode='save',versionname='observatory')

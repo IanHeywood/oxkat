6d118ae107aa2bb2cbcf5ac6f119a42cad3c44a9
IanHeywood <ian.heywood@gmail.com>
2019-08-20 17:45:23 +0100

    cal table flags

---
+++ tools/cal_table_flags.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# ian.heywood@physics.ox.ac.uk
+
+
+import numpy
+import sys
+from pyrap.tables import table
+
+
+def main()
+
+	pat = sys.argv[1]
+
+	tabs = glob.glob(pat)
+
+	for tab in tabs:
+
+		tt = table(tab)
+
+		flags = tt.getcol('FLAG')
+
+		flag_pc = 100.0 * round(flags.sum() / float(flags.size),2)
+
+		print tab,flag_pc
+
+
+if __name__ == "__main__":
+
+
+    main()

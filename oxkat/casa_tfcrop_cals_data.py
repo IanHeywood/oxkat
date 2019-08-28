967e4540fecd00868c9952666aa0191f7ef1d6bb
IanHeywood <ian.heywood@gmail.com>
2019-08-28 01:04:10 +0100

    tfcrop on DATA

---
+++ oxkat/casa_tfcrop_cals_data.py
@@ -0,0 +1,17 @@
+# ian.heywood@physics.ox.ac.uk
+
+
+import pickle
+
+
+project_info = pickle.load(open('project_info.p','rb'))
+myms = project_info['master_ms']
+bpcal = project_info['primary']
+pcal = project_info['secondary']
+
+
+flagdata(vis=myms,mode='tfcrop',datacolumn='data',field=bpcal[1])
+flagdata(vis=myms,mode='tfcrop',datacolumn='data',field=pcal[1])
+
+
+flagmanager(vis=myms,mode='save',versionname='tfcrop_cals')

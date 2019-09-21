4eb6a9d51932210e7e3ce03d297b673f098968c2
IanHeywood <ian.heywood@gmail.com>
2019-09-21 11:15:36 +0100

    Fixed new calibrator setup for flagging scripts

--- oxkat/casa_tfcrop_cals_data.py
+++ oxkat/casa_tfcrop_cals_data.py
@@ -7,7 +7,7 @@ import pickle
 project_info = pickle.load(open('project_info.p','rb'))
 myms = project_info['master_ms']
 bpcal = project_info['primary']
-pcal = project_info['secondary']
+pcals = project_info['secondary']
 
 
 clearstat()
@@ -15,7 +15,9 @@ clearstat()
 
 
 flagdata(vis=myms,mode='tfcrop',datacolumn='data',field=bpcal[1])
-flagdata(vis=myms,mode='tfcrop',datacolumn='data',field=pcal[1])
+for i in range(0,len(pcals)):
+    pcal = pcals[i][1]
+	flagdata(vis=myms,mode='tfcrop',datacolumn='data',field=pcal)
 
 
 flagmanager(vis=myms,mode='save',versionname='tfcrop_cals_data')

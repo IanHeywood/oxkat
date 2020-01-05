0efb08715645fd6c0d79a3a6aaca87e58fb97124
IanHeywood <ian.heywood@gmail.com>
2020-01-05 12:10:44 +0000

    Container updates

--- tools/pull_containers.sh
+++ tools/pull_containers.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 singularity pull docker://stimela/tricolour:dev
-singularity pull docker://stimela/wsclean:1.2.3
-singularity pull docker://stimela/cubical:1.2.3
+singularity pull docker://stimela/wsclean:1.2.4
+singularity pull docker://stimela/cubical:1.2.4
+singularity pull docker://stimela/pybdsf:1.2.4
+singularity pull docker://stimela/ragavi:1.2.4
 singularity pull docker://stimela/codex-africanus:dev
 singularity pull docker://bhugo/ddfacet:0.5.0
 singularity pull docker://bhugo/killms:2.7.0

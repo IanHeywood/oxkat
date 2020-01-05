8aa792a011d7097a0ff954231a887101f502bfda
IanHeywood <ian.heywood@gmail.com>
2020-01-05 12:18:40 +0000

    Plot gaintables

---
+++ tools/plot_gaintables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# ian.heywood@physics.ox.ac.uk
+
+
+import glob
+import os.path as o
+import pickle
+import sys
+sys.path.append(o.abspath(o.join(o.dirname(sys.modules[__name__].__file__), "..")))
+
+
+from oxkat import generate_jobs as gen
+
+
+
+
+def main():
+
+
+	dependency = sys.argv[1]
+
+	caltabs = sorted(glob.glob('cal_*'))
+
+	for caltab in caltabs:
+
+		gaintype = caltab.split('.')[-1][0]
+
+		print gaintype

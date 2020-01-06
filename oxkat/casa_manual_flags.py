a5e98913d070f40931b7dbcc466cc5897fa59d4e
IanHeywood <ian.heywood@gmail.com>
2020-01-06 15:30:38 +0000

    Manual flags

---
+++ oxkat/casa_manual_flags.py
@@ -0,0 +1,44 @@
+# ian.heywood@physics.ox.ac.uk
+
+
+import pickle
+import glob
+
+
+project_info = pickle.load(open('project_info.p','rb'))
+flag_file = glob.glob('*manualflags.txt')
+
+
+myms = project_info['master_ms']
+
+
+clearstat()
+clearstat()
+
+
+f = open(flag_file,'r')
+line = f.readline()
+while line:
+	if line[0] != '#':
+		cols = line.split(':')
+		if len(cols) == 1:
+			ant = cols[0]
+		elif len(cols) == 2:
+			ant = cols[0]
+			scans = cols[1]
+		else:
+			print('Check manual flag instruction:')
+			print(line)
+			continue
+		flagdata(vis=myms,
+			mode = 'manual',
+			selectdata = True,
+			antenna = ant,
+			scan = scans)
+
+
+flagmanager(vis=myms,mode='save',versionname='manual_flags')
+
+
+clearstat()
+clearstat()

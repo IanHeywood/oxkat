ac01e66607d2982c5643428e2a7943a0e10aa01c
IanHeywood <ian.heywood@gmail.com>
2020-04-08 11:05:55 +0100

    Typo fix

--- oxkat/1GC_01_casa_rephase_primary_calibrator.py
+++ oxkat/1GC_01_casa_rephase_primary_calibrator.py
@@ -12,9 +12,9 @@ primary_field = project_info['primary'][1]
 primary_tag = project_info['primary_tag']
 
 
-if primary_tag = '0408':
+if primary_tag == '0408':
     newphasecentre = 'J2000 04h08m20.3782s -65d45m09.080s'
-elif primary_tag = '1934':
+elif primary_tag == '1934':
     newphasecentre = 'J2000 19h39m25.0264s -63d42m45.624s'
 
 

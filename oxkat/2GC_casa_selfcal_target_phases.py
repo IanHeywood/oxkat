734408afc138985e9229661d36504798515423c2
IanHeywood <ian.heywood@gmail.com>
2020-04-16 00:30:54 +0100

    2GC GC mods

--- oxkat/2GC_casa_selfcal_target_phases.py
+++ oxkat/2GC_casa_selfcal_target_phases.py
@@ -1,9 +1,9 @@
 # ian.heywood@physics.ox.ac.uk
 
 
+import pickle
 import sys
 import time
-import shutil
 
 
 execfile('oxkat/config.py')
@@ -12,56 +12,67 @@ execfile('oxkat/config.py')
 def stamp():
     return str(time.time()).replace('.','')
 
+myuvrange = ''
 
 args = sys.argv
 for item in sys.argv:
     parts = item.split('=')
     if parts[0] == 'mslist':
         mslist = parts[1].split(',')
+    if parts[0] == 'uvmin':
+        myuvrange = '>'+parts[1]
 
 
-myuvrange = '>150m'
+if myuvrange == '':
+    myuvrange = '>150m'
 
 
-clearstat()
-clearstat()
+project_info = pickle.load(open('project_info.p','rb'))
+ref_ant = project_info['ref_ant']
 
 
-gtab = GAINTABLES+'/cal_'+myms+'_'+stamp()+'.GP0'
+for myms in mslist:
 
 
-gaincal(vis=myms,
-    field='0',
-    uvrange=myuvrange,
-    caltable=gtab,
-    refant = str(ref_ant),
-    solint='64s',
-    solnorm=False,
-    combine='',
-    minsnr=3,
-    calmode='p',
-    parang=False,
-    gaintable=[],
-    gainfield=[],
-    interp=[],
-    append=False)
+    clearstat()
+    clearstat()
 
 
-applycal(vis=myms,
-    gaintable=[gtab],
-    field='0',
-    calwt=False,
-    parang=False,
-    applymode='calonly',
-    gainfield='0',
-    interp = ['nearest'])
+    gtab = GAINTABLES+'/cal_'+myms+'_'+stamp()+'.GP0'
 
 
-# statwt(vis=myms,
-#     field='0')
+    gaincal(vis=myms,
+        field='0',
+        uvrange=myuvrange,
+        caltable=gtab,
+        refant = str(ref_ant),
+        solint='64s',
+        solnorm=False,
+        combine='',
+        minsnr=3,
+        calmode='p',
+        parang=False,
+        gaintable=[],
+        gainfield=[],
+        interp=[],
+        append=False)
 
 
-clearstat()
-clearstat()
+    applycal(vis=myms,
+        gaintable=[gtab],
+        field='0',
+        calwt=False,
+        parang=False,
+        applymode='calonly',
+        gainfield='0',
+        interp = ['nearest'])
+
+
+    # statwt(vis=myms,
+    #     field='0')
+
+
+    clearstat()
+    clearstat()
 
 

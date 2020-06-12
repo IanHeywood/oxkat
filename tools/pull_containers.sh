c3c24d64cb79f999c525fd46361b90cdffc95916
IanHeywood <ian.heywood@gmail.com>
2020-06-12 18:17:02 +0100

    Container updates

--- tools/pull_containers.sh
+++ tools/pull_containers.sh
@@ -5,7 +5,7 @@ singularity pull docker://stimela/cubical:1.2.5
 #singularity pull docker://bhugo/ddfacet:0.4.1
 singularity pull docker://bhugo/ddfacet:0.5.3.1
 singularity pull docker://bhugo/killms:2.7.0
-singularity pull docker://stimela/meqtrees:1.2.6
+singularity pull docker://stimela/meqtrees:1.2.4
 singularity pull docker://stimela/pybdsf:1.2.6
 singularity pull docker://stimela/ragavi:1.2.6
 singularity pull docker://stimela/shadems:1.7.0

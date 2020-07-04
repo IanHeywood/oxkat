6f680660aaf12c23a4cfd63d6e3e75f62c308594
IanHeywood <ian.heywood@gmail.com>
2020-07-04 18:12:44 +0100

    Tab fix

--- oxkat/1GC_01_casa_rephase_primary_calibrator.py
+++ oxkat/1GC_01_casa_rephase_primary_calibrator.py
@@ -11,12 +11,12 @@ elif primary_tag == '1934':
     newphasecentre = 'J2000 19h39m25.0264s -63d42m45.624s'
     dorephase = True
 else:
-	dorephase = False
+    dorephase = False
 
 if dorephase:
-	fixvis(vis = myms,
-	    field = bpcal,
-	    phasecenter = newphasecentre,
-	    refcode = 'J2000',
-	    datacolumn = 'DATA')
+    fixvis(vis = myms,
+        field = bpcal,
+        phasecenter = newphasecentre,
+        refcode = 'J2000',
+        datacolumn = 'DATA')
 

faf009b4ac65df4d385b837035de3ad7facc1691
IanHeywood <ian.heywood@gmail.com>
2020-11-22 00:14:54 +0000

    New version of 2GC script

--- setups/2GC_new.py
+++ setups/2GC_new.py
@@ -28,7 +28,7 @@ def main():
     IMAGES = cfg.IMAGES
     SCRIPTS = cfg.SCRIPTS
 
-
+    gen.setup_dir(GAINTABLES)
     gen.setup_dir(IMAGES)
     gen.setup_dir(cfg.LOGS)
     gen.setup_dir(cfg.SCRIPTS)
@@ -42,9 +42,8 @@ def main():
 
 
     CASA_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.CASA_PATTERN)
-    DDFACET_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.DDFACET_PATTERN)
     MAKEMASK_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.MAKEMASK_PATTERN)
-    TRICOLOUR_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.TRICOLOUR_PATTERN)
+    RAGAVI_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.RAGAVI_PATTERN)
     WSCLEAN_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.WSCLEAN_PATTERN)
 
 
@@ -59,7 +58,7 @@ def main():
 
     # ------------------------------------------------------------------------------
     #
-    # FLAG recipe definition
+    # 2GC recipe definition
     #
     # ------------------------------------------------------------------------------
 
@@ -85,6 +84,7 @@ def main():
             steps = []        
             filename_targetname = gen.scrub_target_name(targetname)
 
+
             code = gen.get_target_code(targetname)
             if code in codes:
                 print(gen.now()+' Adding suffix to '+targetname+' code to prevent job ID clashes')
@@ -92,74 +92,127 @@ def main():
                 ii += 1
             codes.append(code)
         
-            # Image prefix
-            img_prefix = IMAGES+'/img_'+myms+'_datablind'
+
+            # Look for the FITS mask for this target
+            mask0 = sorted(glob.glob(IMAGES+'/*'+filename_targetname+'*.mask0.fits'))
+            if len(mask0) > 0:
+                mask = mask0[0]
+            else:
+                mask = 'auto'
+
+
+            print('------------------------------------------------------')
+            print(gen.now()+'Target:     '+targetname)
+            print(gen.now()+'MS:         '+myms)
+            print(gen.now()+'Using mask: '+mask)
+
+
+            # Image prefixes
+            data_img_prefix = IMAGES+'/img_'+myms+'_datamask'
+            corr_img_prefix = IMAGES+'/img_'+myms+'_pcalmask'
 
             # Target-specific kill file
-            kill_file = SCRIPTS+'/kill_flag_jobs_'+filename_targetname+'.sh'
+            kill_file = SCRIPTS+'/kill_2GC_jobs_'+filename_targetname+'.sh'
 
 
             step = {}
             step['step'] = 0
-            step['comment'] = 'Run Tricolour on '+myms
+            step['comment'] = 'Run wsclean, masked deconvolution of the DATA column of '+myms
             step['dependency'] = None
-            step['id'] = 'TRICO'+code
-            step['slurm_config'] = cfg.SLURM_TRICOLOUR
-            step['pbs_config'] = cfg.PBS_TRICOLOUR
-            syscall = CONTAINER_RUNNER+TRICOLOUR_CONTAINER+' '
-            syscall += gen.generate_syscall_tricolour(myms = myms,
-                        config = DATA+'/tricolour/target_flagging_1_narrow.yaml',
-                        datacol = 'DATA',
-                        fields = '0',
-                        strategy = 'polarisation')
+            step['id'] = 'WSDMA'+code
+            step['slurm_config'] = cfg.SLURM_WSCLEAN
+            step['pbs_config'] = cfg.PBS_WSCLEAN
+            syscall = CONTAINER_RUNNER+WSCLEAN_CONTAINER+' '
+            syscall += gen.generate_syscall_wsclean(mslist=[myms],
+                        imgname=data_img_prefix,
+                        datacol='DATA',
+                        bda=True,
+                        mask=mask)
             step['syscall'] = syscall
             steps.append(step)
 
 
             step = {}
             step['step'] = 1
-            step['comment'] = 'Blind wsclean on DATA column of '+myms
+            step['comment'] = 'Predict model visibilities from imaging of the DATA column'
             step['dependency'] = 0
-            step['id'] = 'WSDBL'+code
+            step['id'] = 'WSDPR'+code
             step['slurm_config'] = cfg.SLURM_WSCLEAN
             step['pbs_config'] = cfg.PBS_WSCLEAN
             syscall = CONTAINER_RUNNER+WSCLEAN_CONTAINER+' '
-            syscall += gen.generate_syscall_wsclean(mslist = [myms],
-                                    imgname = img_prefix,
-                                    datacol = 'DATA',
-                                    bda = True,
-                                    niter = 90000,
-                                    autothreshold = 3.5,
-                                    mask = 'none')
+            syscall += gen.generate_syscall_predict(msname=myms,imgbase=data_img_prefix)
             step['syscall'] = syscall
             steps.append(step)
 
 
             step = {}
             step['step'] = 2
-            step['comment'] = 'Make initial cleaning mask for '+targetname
+            step['comment'] = 'Run CASA self-calibration script'
             step['dependency'] = 1
-            step['id'] = 'MASK0'+code
-            syscall = CONTAINER_RUNNER+MAKEMASK_CONTAINER+' '
-            syscall += gen.generate_syscall_makemask(restoredimage = img_prefix+'-MFS-image.fits',
-                                    outfile = img_prefix+'-MFS-image.mask0.fits',
-                                    zoompix = '')[0]
+            step['id'] = 'CL2GC'+code
+            syscall = CONTAINER_RUNNER+CASA_CONTAINER+' '
+            syscall += gen.generate_syscall_casa(casascript=OXKAT+'/2GC_casa_selfcal_target_amp_phases.py',
+                        casalogfile=casalog,
+                        extra_args='mslist='+myms)
             step['syscall'] = syscall
             steps.append(step)
 
 
             step = {}
             step['step'] = 3
-            step['comment'] = 'Backup flag table for '+myms
-            step['dependency'] = 1
-            step['id'] = 'SAVFG'+code
-            syscall = CONTAINER_RUNNER+CASA_CONTAINER+' '
-            syscall += 'casa -c '+OXKAT+'/FLAG_casa_backup_flag_table.py --nologger --log2term --nogui '
-            syscall += 'versionname=tricolour1 '
+            step['comment'] = 'Plot the self-calibration gain solution'
+            step['dependency'] = 2
+            step['id'] = 'PLTAB'+code
+            syscall = CONTAINER_RUNNER+RAGAVI_CONTAINER+' '
+            syscall += 'python3 '+OXKAT+'/PLOT_gaintables.py cal_2GC_*'+myms+'*'
             step['syscall'] = syscall
             steps.append(step)
 
+
+            step = {}
+            step['step'] = 4
+            step['comment'] = 'Run wsclean, masked deconvolution of the CORRECTED_DATA column of '+myms
+            step['dependency'] = 2
+            step['id'] = 'WSCMA'+code
+            syscall = CONTAINER_RUNNER+WSCLEAN_CONTAINER+' '
+            syscall += gen.generate_syscall_wsclean(mslist=[myms],
+                        imgname=corr_img_prefix,
+                        datacol='CORRECTED_DATA',
+                        bda=True,
+                        mask=mask)
+            step['syscall'] = syscall
+
+
+            step = {}
+            step['step'] = 5
+            step['comment'] = 'Refine the cleaning mask for '+targetname+', crop for use with DDFacet'
+            step['dependency'] = 4
+            step['id'] = 'MASK1'+code
+            syscall = CONTAINER_RUNNER+MAKEMASK_CONTAINER+' '
+            syscall += gen.generate_syscall_makemask(restoredimage = corr_img_prefix+'-MFS-image.fits',
+                                    outfile = corr_img_prefix+'-MFS-image.mask1.fits',
+                                    thresh = 5.5,
+                                    zoompix = cfg.DDF_NPIX)[0]
+            step['syscall'] = syscall
+            steps.append(step)
+
+
+            step = {}
+            step['step'] = 6
+            step['comment'] = 'Predict model visibilities from imaging of the CORRECTED_DATA column'
+            step['dependency'] = 5
+            step['id'] = 'WSDPR'+code
+            step['slurm_config'] = cfg.SLURM_WSCLEAN
+            step['pbs_config'] = cfg.PBS_WSCLEAN
+            syscall = CONTAINER_RUNNER+WSCLEAN_CONTAINER+' '
+            syscall += gen.generate_syscall_predict(msname=myms,imgbase=corr_img_prefix)
+            step['syscall'] = syscall
+            steps.append(step)
+
+
             target_steps.append((steps,kill_file,targetname))
+
+
 
 
     # ------------------------------------------------------------------------------
@@ -169,7 +222,7 @@ def main():
     # ------------------------------------------------------------------------------
 
 
-    submit_file = 'submit_flag_jobs.sh'
+    submit_file = 'submit_2GC_jobs.sh'
 
     f = open(submit_file,'w')
     f.write('#!/usr/bin/env bash\n')

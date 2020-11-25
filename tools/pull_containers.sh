fda52e95d338de4b7ccaef967c77ed0ccc584cd2
IanHeywood <ian.heywood@gmail.com>
2020-11-25 15:12:28 +0000

    Fix PyBDSF container version

--- tools/pull_containers.sh
+++ tools/pull_containers.sh
@@ -8,7 +8,7 @@ singularity pull docker://stimela/ddfacet:1.3.3
 singularity pull docker://bhugo/killms:2.7.0
 singularity pull docker://stimela/meqtrees:1.6.3
 singularity pull docker://stimela/owlcat:1.6.6
-singularity pull docker://stimela/pybdsf:1.2.6
+singularity pull docker://stimela/pybdsf:1.6.6
 singularity pull docker://stimela/ragavi:1.7.3
 singularity pull docker://stimela/shadems:1.7.1
 singularity pull docker://stimela/tricolour:1.5.0

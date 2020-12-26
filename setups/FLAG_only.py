5f634e4b9843ea37a82f703e8d5eb568bb41b7cd
IanHeywood <ian.heywood@gmail.com>
2020-12-26 21:19:12 +0000

    Setup script for only performing flagging

---
+++ setups/FLAG_only.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# ian.heywood@physics.ox.ac.uk
+
+
+import glob
+import os.path as o
+import pickle
+import sys
+sys.path.append(o.abspath(o.join(o.dirname(sys.modules[__name__].__file__), "..")))
+
+
+from oxkat import generate_jobs as gen
+from oxkat import config as cfg
+
+
+def main():
+
+    USE_SINGULARITY = cfg.USE_SINGULARITY
+
+    gen.preamble()
+    print(gen.now()+'FLAG (flagging & initial mask-making) setup')
+
+
+    # ------------------------------------------------------------------------------
+    #
+    # Setup paths, required containers, infrastructure
+    #
+    # ------------------------------------------------------------------------------
+
+
+    OXKAT = cfg.OXKAT
+    DATA = cfg.DATA
+    SCRIPTS = cfg.SCRIPTS
+    TOOLS = cfg.TOOLS
+
+
+    gen.setup_dir(cfg.LOGS)
+    gen.setup_dir(cfg.SCRIPTS)
+
+
+    INFRASTRUCTURE, CONTAINER_PATH = gen.set_infrastructure(sys.argv)
+    if CONTAINER_PATH is not None:
+        CONTAINER_RUNNER='singularity exec '
+    else:
+        CONTAINER_RUNNER=''
+
+
+    CASA_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.CASA_PATTERN,USE_SINGULARITY)
+    TRICOLOUR_CONTAINER = gen.get_container(CONTAINER_PATH,cfg.TRICOLOUR_PATTERN,USE_SINGULARITY)
+
+
+    # Get target information from project pickle
+
+    project_info = pickle.load(open('project_info.p','rb'),encoding='latin1')
+
+    target_ids = project_info['target_ids'] 
+    target_names = project_info['target_names']
+    target_ms = project_info['target_ms']
+
+
+    # ------------------------------------------------------------------------------
+    #
+    # FLAG recipe definition
+    #
+    # ------------------------------------------------------------------------------
+
+
+    target_steps = []
+    codes = []
+    ii = 1
+
+    # Loop over targets
+
+    for tt in range(0,len(target_ids)):
+
+        targetname = target_names[tt]
+        myms = target_ms[tt]
+
+        if not o.isdir(myms):
+
+            gen.print_spacer()
+            print(gen.now()+'Target    | '+targetname)
+            print(gen.now()+'MS        | not found, skipping')
+
+        else:
+
+            steps = []        
+            filename_targetname = gen.scrub_target_name(targetname)
+
+            code = gen.get_target_code(targetname)
+            if code in codes:
+                code += '_'+str(ii)
+                ii += 1
+            codes.append(code)
+        
+            # Image prefix
+            img_prefix = IMAGES+'/img_'+myms+'_datablind'
+
+            # Target-specific kill file
+            kill_file = SCRIPTS+'/kill_flag_jobs_'+filename_targetname+'.sh'
+
+            gen.print_spacer()
+            print(gen.now()+'Target    | '+targetname)
+            print(gen.now()+'MS        | '+myms)
+
+            step = {}
+            step['step'] = 0
+            step['comment'] = 'Run Tricolour on '+myms
+            step['dependency'] = None
+            step['id'] = 'TRIC0'+code
+            step['slurm_config'] = cfg.SLURM_TRICOLOUR
+            step['pbs_config'] = cfg.PBS_TRICOLOUR
+            syscall = CONTAINER_RUNNER+TRICOLOUR_CONTAINER+' ' if USE_SINGULARITY else ''
+            syscall += gen.generate_syscall_tricolour(myms = myms,
+                        config = DATA+'/tricolour/target_flagging_1_narrow.yaml',
+                        datacol = 'DATA',
+                        fields = '0',
+                        strategy = 'polarisation')
+            step['syscall'] = syscall
+            steps.append(step)
+
+
+            step = {}
+            step['step'] = 1
+            step['comment'] = 'Backup flag table for '+myms
+            step['dependency'] = 0
+            step['id'] = 'SAVFG'+code
+            syscall = CONTAINER_RUNNER+CASA_CONTAINER+' ' if USE_SINGULARITY else ''
+            syscall += 'casa -c '+OXKAT+'/FLAG_casa_backup_flag_table.py --nologger --log2term --nogui '
+            syscall += 'versionname=tricolour1 mslist='+myms
+            step['syscall'] = syscall
+            steps.append(step)
+            
+
+            target_steps.append((steps,kill_file,targetname))
+
+
+    # ------------------------------------------------------------------------------
+    #
+    # Write the run file and kill file based on the recipe
+    #
+    # ------------------------------------------------------------------------------
+
+
+    submit_file = 'submit_flag_jobs.sh'
+
+    f = open(submit_file,'w')
+    f.write('#!/usr/bin/env bash\n')
+    f.write('export SINGULARITY_BINDPATH='+cfg.BINDPATH+'\n')
+
+    for content in target_steps:  
+        steps = content[0]
+        kill_file = content[1]
+        targetname = content[2]
+        id_list = []
+
+        f.write('\n#---------------------------------------\n')
+        f.write('# '+targetname)
+        f.write('\n#---------------------------------------\n')
+
+        for step in steps:
+
+            step_id = step['id']
+            id_list.append(step_id)
+            if step['dependency'] is not None:
+                dependency = steps[step['dependency']]['id']
+            else:
+                dependency = None
+            syscall = step['syscall']
+            if 'slurm_config' in step.keys():
+                slurm_config = step['slurm_config']
+            else:
+                slurm_config = cfg.SLURM_DEFAULTS
+            if 'pbs_config' in step.keys():
+                pbs_config = step['pbs_config']
+            else:
+                pbs_config = cfg.PBS_DEFAULTS
+            comment = step['comment']
+
+            run_command = gen.job_handler(syscall = syscall,
+                            jobname = step_id,
+                            infrastructure = INFRASTRUCTURE,
+                            dependency = dependency,
+                            slurm_config = slurm_config,
+                            pbs_config = pbs_config)
+
+
+            f.write('\n# '+comment+'\n')
+            f.write(run_command)
+
+        if INFRASTRUCTURE != 'node':
+            f.write('\n# Generate kill script for '+targetname+'\n')
+        if INFRASTRUCTURE == 'idia' or INFRASTRUCTURE == 'hippo':
+            kill = 'echo "scancel "$'+'" "$'.join(id_list)+' > '+kill_file+'\n'
+            f.write(kill)
+        elif INFRASTRUCTURE == 'chpc':
+            kill = 'echo "qdel "$'+'" "$'.join(id_list)+' > '+kill_file+'\n'
+            f.write(kill)
+
+        
+    f.close()
+
+    gen.make_executable(submit_file)
+
+    gen.print_spacer()
+    print(gen.now()+'Created '+submit_file)
+    gen.print_spacer()
+
+    # ------------------------------------------------------------------------------
+
+
+
+if __name__ == "__main__":
+
+
+    main()

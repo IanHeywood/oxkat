2891ab1e6ed5d094e32b5058bb1144d49ce9d995
IanHeywood <ian.heywood@gmail.com>
2021-10-25 22:30:02 +0100

    ms_info and scan_times in 1GC

--- setups/1GC.py
+++ setups/1GC.py
@@ -88,7 +88,11 @@ def main():
         step['dependency'] = 0
         step['id'] = 'SETUP'+code
         syscall = CONTAINER_RUNNER+MEQTREES_CONTAINER+' ' if USE_SINGULARITY else ''
-        syscall += ' python '+cfg.OXKAT+'/1GC_00_setup.py '+myms
+        syscall += ' python '+cfg.OXKAT+'/1GC_00_setup.py '+myms+'\n '
+        syscall = CONTAINER_RUNNER+MEQTREES_CONTAINER+' ' if USE_SINGULARITY else ''
+        syscall += ' python '+cfg.TOOLS+'/ms_info.py '+myms+'\n '
+        syscall = CONTAINER_RUNNER+MEQTREES_CONTAINER+' ' if USE_SINGULARITY else ''
+        syscall += ' python '+cfg.TOOLS+'/scan_times.py '+myms
         step['syscall'] = syscall
         steps.append(step)
 

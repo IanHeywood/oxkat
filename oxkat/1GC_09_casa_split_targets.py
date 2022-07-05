dfa2e93847144baa6e812ff0a43e5dd7ecc7b92d
IanHeywood <ian.heywood@gmail.com>
2022-07-05 20:31:15 +0100

    Fix egregious target splitting bug

--- oxkat/1GC_09_casa_split_targets.py
+++ oxkat/1GC_09_casa_split_targets.py
@@ -7,20 +7,28 @@ execfile('oxkat/config.py')
 if PRE_FIELDS != '':
     targets = user_targets
 
-for target in targets:
+for target in target_names:
+
+    opms = ''
 
     for mm in target_ms:
         if target in mm:
             opms = mm
 
-    mstransform(vis=myms,
-        outputvis=opms,
-        field=target,
-        usewtspectrum=True,
-        realmodelcol=True,
-        datacolumn='corrected')
+    if opms != '':
 
-    if SAVE_FLAGS:
-        flagmanager(vis=opms,
-            mode='save',
-            versionname='post-1GC')
+        mstransform(vis=myms,
+            outputvis=opms,
+            field=target,
+            usewtspectrum=True,
+            realmodelcol=True,
+            datacolumn='corrected')
+
+        if SAVE_FLAGS:
+            flagmanager(vis=opms,
+                mode='save',
+                versionname='post-1GC')
+
+    else:
+
+        print('Target/MS mismatch in project info for '+target+', please check.')
